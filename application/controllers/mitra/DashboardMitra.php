db09b14349d880313685cbda494d1bbb82c7490a
priskachoirina <priska_choirina@yahoo.com>
2019-12-21 09:34:51 +0700

    update mitra

--- application/controllers/mitra/DashboardMitra.php
+++ application/controllers/mitra/DashboardMitra.php
@@ -13,7 +13,7 @@ class DashboardMitra extends MY_Controller {
         $this->data['title']    = 'DashboardMitra';
         $this->data['key']      = 'dashboardmitra';
         $this->data['menu']     = 'template/menu_mitra';
-        $this->data['logout']   = 'PanelMitra/logout';
+        $this->data['logout']   = 'mitra/PanelMitra/logout';
     }
 
     public function getInfoTrans()
@@ -47,71 +47,10 @@ class DashboardMitra extends MY_Controller {
     
     public function index()
     { 
-        $this->data['info'] = array();
-        if(isset($_COOKIE['token_company'])){
-            $this->data['nameCom'] = $this->getCompanySign();
-            $this->data['list']  = $this->apilib->assignstore('GET')['data'];  
-
-            if(empty($this->data['list'])){
-                $this->data['msg']   = 'List Store Kosong';
-            }else{
-                $this->data['msg']   = 'Pilih Store untuk melakukan aktivitas pada sistem ini.';
-            }
-            $this->data['form']  = 'signinstore'; 
-        }else{
-            $this->data['list']  = $this->apilib->company('GET')['data'];  
-            if(empty($this->data['list'])){
-                $this->data['msg']   = 'List Company Kosong';
-            }else{
-                $this->data['msg']   = 'Pilih Company untuk melakukan aktivitas pada sistem ini.';
-            } 
-            $this->data['form']  = 'signincompany';
-        }  
-
-        if(isset($_COOKIE['token_store'])){
-            $this->data['investor'] = $this->apilib->investor('GET')['data'];  
-            $this->data['info']     = $this->getInfoTrans();
-        }
+        $this->data['me'] =  $this->apilib->me('GET')['data'][0];
 
 		$this->view('mitra/dashboard_mitra_v',$this->data);
-    } 
-
-    public function signincompany($id)
-    { 
-        if(isset($id)){ 
-            $company = $this->apilib->checkinowncompany(array('company_id'=>$id));
-             
-            setcookie("token_company", $company['data'][0]['newToken'], time() + (86400 * 30), "/");  
-            setcookie("company_sign", $id, time() + (86400 * 30), "/");  
-
-            redirect(base_url().'mitra/'.$this->page);
-        }
-
-    }
-
-    public function signinstore($id)
-    {
-        if(isset($id)){
-            // get token store
-            $t_store = $this->apilib->checkinownstore(array('stores_id'=> $id));
-
-            setcookie("token_store", $t_store['data'][0]['newToken'], time() + (86400 * 30), "/");  
-            setcookie("store_sign", $id, time() + (86400 * 30), "/");  
-        }
-        
-        redirect(base_url().'mitra/'.$this->page);
-    }
-
-    public function change_store()
-    {
-        if(isset($_COOKIE['token_store'])){ 
-            unset($_COOKIE['token_store']); 
-            unset($_COOKIE['store_sign']); 
-            setcookie('token_store', null, -1, '/'); 
-            setcookie('store_sign', null, -1, '/'); 
-        }
-        redirect(base_url().'mitra/'.$this->page);
-    }
+    }  
 }
 
 /* End of file Kloter.php */
--- application/views/mitra/dashboard_mitra_v.php
+++ application/views/mitra/dashboard_mitra_v.php
@@ -22,93 +22,9 @@
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-            <?php if (!$storetkn) { ?>
-                <div class="col-md-12">
-                    <div class="callout callout-info">
-                        <h5><i class="fas fa-info"></i> Note:</h5>  
-                        <?php echo $msg ?>
-                        
-                    </div>
-                </div> 
-                <?php foreach ($list as $key => $value) { ?>
-                    <div class="col-12 col-sm-6 col-md-3">
-                        <a href="<?php echo base_url().'mitra/DashboardMitra/'.$form.'/'.$value['id']?>">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-store-alt"></i></span>
-
-                                <div class="info-box-content">
-                                    <span class="info-box-text"><?php echo $value['name']?></span>
-                                    <span class="info-box-number">
-                                    
-                                    <small></small>
-                                    </span>
-                                </div>
-                                <!-- /.info-box-content -->
-                            </div>
-                            <!-- /.info-box -->
-                        </a>
-                    </div>
-                <?php } ?>
-            <?php }else{ ?>
-                <div class="col-md-12">
-                    <div class="callout callout-info">
-                        <h5><i class="fas fa-store-alt"></i> Signin pada Store : <?php echo $sign_store['name'] ?></h5>
-                        <?php echo $msg ?>
-                    </div>
-                </div> 
-            <?php } ?>
-            <?php if(!empty($info)){ ?>
             <div class="col-md-12">
-                <div class="card">
-                <div class="card-header border-transparent">
-                    <h3 class="card-title">List Transaction</h3>
-
-                    <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove">
-                        <i class="fas fa-times"></i>
-                    </button>
-                    </div>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body p-0">
-                    <?php print_arr($investor);   ?>
-                    <div class="table-responsive">
-                        <table class="table m-0">
-                            <thead>
-                                <tr>
-                                    <th>Tanggal</th>
-                                    <th>Jumlah</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php 
-                                    if(isset($info['countdata'])){
-                                    foreach ($info['countdata'] as $key => $value) { 
-                                ?>
-                                <tr>
-                                    <td><?php echo $value['date'][0] ?></td>
-                                    <td><?php echo $value['sum'][0] ?></td>
-                                </tr>
-                                <?php 
-                                    } 
-                                }
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.table-responsive -->
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer clearfix">
-                   
-                </div>
-                <!-- /.card-footer -->
-                </div>
+                <?php print_arr($me); ?>
             </div>
-            <?php } ?>
         </div>
       </div>
     </section>
